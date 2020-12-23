1cb703996e24a1bcf873330f826c147740be8fa5
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2020-12-24 00:53:21 +0900

    Add Validation Logic

--- src/main/java/com/example/todolist/common/controller/RestExceptionHandler.java
+++ src/main/java/com/example/todolist/common/controller/RestExceptionHandler.java
@@ -3,10 +3,14 @@ package com.example.todolist.common.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
@@ -15,7 +19,7 @@ import java.util.NoSuchElementException;
 public class RestExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<Map<String, Object>> notFoundHandler(NoSuchElementException e) {
+    public ResponseEntity<Map<String, Object>> handleNotFound(NoSuchElementException e) {
         Map<String, Object> body = new HashMap<>();
 
         body.put("Status Code", "404 Not Found");
@@ -26,7 +30,7 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Map<String, Object>> badRequestHandler(IllegalArgumentException e) {
+    public ResponseEntity<Map<String, Object>> handleBadRequest(IllegalArgumentException e) {
         Map<String, Object> body = new HashMap<>();
 
         body.put("Status Code", "400 Bad Request");
@@ -37,8 +41,19 @@ public class RestExceptionHandler {
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(value = MethodArgumentNotValidException.class) // 유효성 검사 실패 시 발생하는 예외를 처리
+    public ResponseEntity<Map<String, Object>> handleInvalidException(MethodArgumentNotValidException e) {
+
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("Status Code", "400 Bad Request");
+        body.put("errorMsg", getResultMessage(e.getBindingResult().getFieldErrors().iterator()));
+
+        return ResponseEntity.badRequest().body(body);
+    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, Object>> internalServerErrorHandler(Exception e) {
+    public ResponseEntity<Map<String, Object>> handleInternalServerError(Exception e) {
         Map<String, Object> body = new HashMap<>();
 
         body.put("Status Code", "500 Internal Server Error");
@@ -46,5 +61,31 @@ public class RestExceptionHandler {
         log.error("{}", e);
 
         return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    protected String getResultMessage(final Iterator<FieldError> it) {
+        final StringBuilder msgBuilder = new StringBuilder();
+
+        while (it.hasNext()) {
+            final FieldError fieldError = it.next();
+            msgBuilder
+                    .append("['")
+                    .append(fieldError.getField())
+                    .append("' is '")
+                    .append(fieldError.getRejectedValue())
+                    .append("'. ")
+                    .append(fieldError.getDefaultMessage())
+                    .append("]");
+
+            if (it.hasNext()) {
+                msgBuilder.append(", ");
+            }
+        }
+
+        return msgBuilder.toString();
+    }
+
+    protected String getPropertyName(String propertyPath) {
+        return propertyPath.substring(propertyPath.lastIndexOf('.') + 1); // 전체 속성 경로에서 속성 이름만 가져온다.
     }
 }
