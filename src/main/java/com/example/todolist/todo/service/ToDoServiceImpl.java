a8db5c7c8a234c6b87c3075c424324c2f635fca1
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2020-12-24 19:36:35 +0900

    Change business logic of ToDoServiceImpl

--- src/main/java/com/example/todolist/todo/service/ToDoServiceImpl.java
+++ src/main/java/com/example/todolist/todo/service/ToDoServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.todolist.todo.service;
 
+import com.example.todolist.list.entity.ToDoList;
+import com.example.todolist.list.repository.ToDoListRepository;
 import com.example.todolist.todo.dto.CreateToDoRequest;
 import com.example.todolist.todo.dto.ToDoResponse;
 import com.example.todolist.todo.dto.UpdateToDoRequest;
 import com.example.todolist.todo.entity.ToDo;
 import com.example.todolist.todo.repository.ToDoRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
@@ -17,11 +20,19 @@ import static java.util.stream.Collectors.toList;
 @AllArgsConstructor
 @Service
 @Transactional
+@Slf4j
 public class ToDoServiceImpl implements ToDoService {
     private final ToDoRepository toDoRepository;
+    private final ToDoListRepository toDoListRepository;
 
     public ToDoResponse insertToDo(CreateToDoRequest toDoDto) {
-        ToDo toDo = toDoRepository.save(toDoDto.toEntity());
+        ToDoList list = toDoListRepository.findOneById(toDoDto.getListId())
+                .orElseThrow();
+
+        ToDo toDo = toDoDto.toEntity();
+        toDo.setToDoList(list);
+
+        toDo = toDoRepository.save(toDo);
 
         return ToDoResponse.of(toDo);
     }
@@ -42,10 +53,10 @@ public class ToDoServiceImpl implements ToDoService {
     }
 
     public ToDoResponse updateToDo(UpdateToDoRequest toDoDto) {
-        toDoRepository.findOneById(toDoDto.getId())
+        ToDo toDo = toDoRepository.findOneById(toDoDto.getId())
                 .orElseThrow();
 
-        ToDo toDo = toDoRepository.save(toDoDto.toEntity());
+        toDo.update(toDoDto.toEntity());
 
         return ToDoResponse.of(toDo);
     }
